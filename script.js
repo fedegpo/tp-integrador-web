847033fc0c7bec483d84ffa39acc34a190dcd8e3
Escobar Stefani <stefaninairescobar@gmail.com>
2024-10-24 05:18:53 -0300

    Agregue estilos, diseños , nuevas funciones.
    Agregue estilos, diseño, y agregue algunas cositas nuevas!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let images = ["images/bike.png", "images/bike-2.webp", "images/bike-3.png", "images/bike-4.jpg", "images/bike-5.jpg"];
+let images = ["images/file (1).png", "images/bike-2.webp", "images/file (2).png","images/bike-3.png","images/file (3).png", "images/file (7).png"];
 let currentIndex = 0;
 
 function showImage(index) {
@@ -19,18 +19,30 @@ function prevImage() {
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
+    let nombre = document.getElementById('nombre').value;
     let email = document.getElementById('email').value;
     let telefono = document.getElementById('telefono').value;
     let mensaje = document.getElementById('mensaje').value;
 
-    if (validarEmail(email) && validarTelefono(telefono)) {
-        let result = `Correo: ${email}, Teléfono: ${telefono}, Mensaje: ${mensaje}`;
+    if (validarEmail(email) && validarTelefono(telefono) && validarNombre(nombre)) {
+        let result = `¡Gracias por su mensaje! Recibimos los siguientes datos: Nombre y Apellido: ${nombre}, Correo: ${email}, Teléfono: ${telefono}, Mensaje: ${mensaje}`;
         document.getElementById('form-result').textContent = result;
         document.getElementById('form-result').style.color = 'green';
     } else {
+       /* document.getElementById('nombreError').style.display = 'block';
+        document.getElementById('emailError').style.display = 'block';
+        document.getElementById('telefonoError').style.display = 'block';
+        /*Limpiar formulario
+        nombre.value="";
+        email.value="";
+        telefono.value="";
+        mensaje.value="";*/
+        
         document.getElementById('form-result').textContent = 'Error en los datos ingresados.';
         document.getElementById('form-result').style.color = 'red';
     }
+    
+
 });
 
 function validarEmail(email) {
@@ -41,4 +53,9 @@ function validarEmail(email) {
 function validarTelefono(telefono) {
     let regex = /^[0-9]{10}$/;
     return regex.test(telefono);
+}
+
+function validarNombre(nombre) {
+    let regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]{1,50}$/;
+    return regex.test(nombre);
 }
